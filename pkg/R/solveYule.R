3aa8518d884f56ec61f5a9c23b42d9cbf15bc188
jrathjens <jrathjens@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-13 16:55:03 +0000

    Yule-Walker function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robts@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/solveYule.R
@@ -0,0 +1,23 @@
+## solveYule - calculation of AR model coefficients by solving the Yule-Walker equations
+## input:
+## 	ts: time series
+## 	acf: autocorrelation function calculated from ts
+## 	p: order of the AR model
+## 	robvar: should the variance be calculated robustly?
+## output: AR model coefficients
+
+solveYule <- function(ts, acf, p, robvar = TRUE) {
+	p <- as.integer(p)
+	stopifnot(is.numeric(acf), is.numeric(ts), p >= 1, is.logical(robvar))
+	if (length(acf) < p) stop("lags up to order of AR model required")
+	if (robvar) s <- mad(ts) else s <- sd(ts)
+	# autocovariance function:
+	acovf <- s^2 * acf[1:p]
+	CC <- diag(x = s^2, nrow = p)
+	if (p > 1) for (i in 1:(p-1)) {
+		CC[i, (i+1):p] <- acovf[1:(p-i)]
+		CC[p-i+1, 1:(p-i)] <- acovf[(p-i):1]
+	}
+	ph <- solve(a = CC, b = acovf)
+	return(ph)
+}
