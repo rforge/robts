606ae76ee4f141c1bb06c29384e29bb0f394e1be
jrathjens <jrathjens@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-11-19 14:59:22 +0000

    intercept optional
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robts@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/lmrobAR.R
+++ pkg/R/lmrobAR.R
@@ -2,24 +2,25 @@
 ## Input:
 ##		ts: time series
 ## 		p: order of the AR model
+##		interc: with intercept?
 ##		arguments to be passed to lmrob
 ## Output: list of 2:
-## 		coefficients: vector of coefficients with intercept in the last place
+## 		coefficients: vector of coefficients (with intercept in the last place)
 ##		model: object of class lmrob
 
 
-lmrobAR <- function(ts, p, method = "MM", singular.ok = FALSE, init = NULL) {
+lmrobAR <- function(ts, p, interc = TRUE, method = "MM", singular.ok = FALSE, init = NULL) {
 	p <- as.integer(p)
 	stopifnot(is.numeric(ts))
 	tmax <- length(ts)
 	n <- tmax - p
-	if (n < p + 1) stop("Not enough data: lesser order of AR model required.")
+	if (n < p + as.numeric(interc) + 1) stop("Not enough data: lesser order of AR model required.")
 	y <- ts[(p + 1):tmax]
-	G <- matrix(data = 1, nrow = n, ncol = p + 1)
+	G <- matrix(data = 1, nrow = n, ncol = p + as.numeric(interc))
 	for (i in 1:p) G[, p + 1 - i] <- ts[i:(i + n - 1)]
 	lmo <- lmrob(formula = y ~ 0 + G, method = method, singular.ok = singular.ok, init = init)
 	be <- lmo$coefficients
 	for (i in 1:p) names(be)[i] <- paste("phi", i)
-	names(be)[p + 1] <- "interc"
+	if (interc) names(be)[p + 1] <- "interc"
 	return(list(coefficients = be, model = lmo))
 }
--- pkg/R/lmrobARopt.R
+++ pkg/R/lmrobARopt.R
@@ -1,21 +1,24 @@
 ## lmrobARopt - optimally fitted AR model with respect to adjusted R-squared
 ## Input:
 ##		ts: time series
+##		interc: with intercept?
 ##		##		arguments to be passed to lmrob
 ## Output: list of 2:
-## 		coefficients: vector of coefficients with intercept in the last place
+## 		coefficients: vector of coefficients (with intercept in the last place)
 ##		model: object of class lmrob
 ##		p.optimal
 
 
-lmrobARopt <- function(ts, method = "MM", singular.ok = FALSE, init = NULL) {
+lmrobARopt <- function(ts, interc = TRUE, method = "MM", singular.ok = FALSE, init = NULL) {
 	tmax <- length(ts)
-	if (tmax < 7) stop("Too less data for reasonable model comparison. Try p = 1.")
-	pmax <- floor((tmax - 1) / 2) - 1 # AIC implausible small for utmost p
+	o1 <- as.numeric(interc)
+	if (tmax < 5 + o1) stop("Too less data for reasonable model comparison. Try p = 1.")
+	pmax <- floor((tmax - 1 - o1) / 2) - 1
 	aicbest <- +Inf
 	popt <- 0
 	for (p in 1:pmax) {
-		fit <- suppressWarnings(lmrobAR(ts = ts, p = p, method = method, singular.ok = singular.ok, init = init)$model)
+		fit <- suppressWarnings(lmrobAR(ts = ts, p = p, interc = interc,
+			method = method, singular.ok = singular.ok, init = init)$model)
 		if (fit$converged) {
 			aicnew <- log(sum((fit$residuals)^2) / (tmax - p)) + 2 * p / (tmax - p)
 			if (aicnew < aicbest) {
@@ -28,6 +31,6 @@ lmrobARopt <- function(ts, method = "MM", singular.ok = FALSE, init = NULL) {
 	if (popt == 0) stop("No convergence for any p.")
 	be <- fitbest$coefficients
 	for (i in 1:popt) names(be)[i] <- paste("phi", i)
-	names(be)[popt + 1] <- "interc"
+	if (interc) names(be)[popt + 1] <- "interc"
 	return(list(coefficients = be, model = fitbest, p.optimal = popt))
 }
