3ff8f1348f6e2b1ba0528dd272529b34b70cc2cf
jrathjens <jrathjens@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-08 10:21:36 +0000

    variance estimate canceled
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robts@66 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/DurbinLev.R
+++ pkg/R/DurbinLev.R
@@ -1,27 +1,24 @@
 ## DurbinLev - partial autocorrelation function by Durbin-Levinson algorithm
 ## input:
-## 		ts: time series
-## 		acf: autocorrelation function calculated from ts
-## 		robvar: should the variance be calculated robustly?
+## 		acf: autocorrelation function
 ## output: list of 2:
 ##		phis: list of coefficient vectors
 ##		nus: vector of mean squared errors
 
-DurbinLev <- function(ts, acf, robvar = TRUE) {
-	stopifnot(is.numeric(acf), is.numeric(ts), is.logical(robvar))
-	if (robvar) s <- scaleTau2(ts) else s <- sd(ts)
-	# autocovariance function from 0:
-	gammas <- s^2 * c(1, acf)
+DurbinLev <- function(acf) {
+	stopifnot(is.numeric(acf))
+	# autocorrelation function from 0:
+	gammas <- c(1, acf)
 	n <- length(gammas) - 1
 	if (n < 1) stop("autocorrelation of lag 1 required at least")
 	nus <- numeric(n + 1)
 	phis <- vector("list", n)
-	phis[[1]] <- gammas[2] / gammas[1]
-	nus[1] <- gammas[1]
+	phis[[1]] <- gammas[2]
+	nus[1] <- 1
 	nus[2] <- nus[1] * (1 - phis[[1]][1]^2)
 	if(n >= 1) {
 		for(i in 2:n) {
-			phi_ii <- (gammas[i + 1] - sum(phis[[i - 1]] * gammas[i - 0:(i - 2)])) * 1/nus[i]
+			phi_ii <- (gammas[i + 1] - sum(phis[[i - 1]] * gammas[i - 0:(i - 2)])) / nus[i]
 			phis[[i]] <- c(phis[[i - 1]] - phi_ii * rev(phis[[i - 1]]), phi_ii)
 			nus[i + 1] <- nus[i] * (1 - phi_ii^2)
 		}
--- pkg/R/solveYule.R
+++ pkg/R/solveYule.R
@@ -1,23 +1,20 @@
 ## solveYule - calculation of AR model coefficients by solving the Yule-Walker equations
 ## input:
-## 	ts: time series
-## 	acf: autocorrelation function calculated from ts
+## 	acf: autocorrelation function
 ## 	p: order of the AR model
-## 	robvar: should the variance be calculated robustly?
 ## output: AR model coefficients
 
-solveYule <- function(ts, acf, p, robvar = TRUE) {
+solveYule <- function(acf, p) {
 	p <- as.integer(p)
-	stopifnot(is.numeric(acf), is.numeric(ts), p >= 1, is.logical(robvar))
+	stopifnot(is.numeric(acf), p >= 1)
 	if (length(acf) < p) stop("lags up to order of AR model required")
-	if (robvar) s <- scaleTau2(ts) else s <- sd(ts)
-	# autocovariance function:
-	acovf <- s^2 * acf[1:p]
-	CC <- diag(x = s^2, nrow = p)
+	# autocorrelation function:
+	acorf <- acf[1:p]
+	CC <- diag(nrow = p)
 	if (p > 1) for (i in 1:(p-1)) {
-		CC[i, (i+1):p] <- acovf[1:(p-i)]
-		CC[p-i+1, 1:(p-i)] <- acovf[(p-i):1]
+		CC[i, (i+1):p] <- acorf[1:(p-i)]
+		CC[p-i+1, 1:(p-i)] <- acorf[(p-i):1]
 	}
-	ph <- solve(a = CC, b = acovf)
+	ph <- solve(a = CC, b = acorf)
 	return(ph)
 }
