d2255633c4d896831f9b2874799abf4d2f20c087
jrathjens <jrathjens@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-03-27 11:04:02 +0000

    non-robust option in robspectrum
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robts@164 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/robspectrum.R
+++ pkg/R/robspectrum.R
@@ -37,7 +37,7 @@ robspectrum <- function(x, method = c("pgram", "ar"), psifunc = smoothpsi, trunc
 	}
 	
 	if (method == "ar") {
-		if (acf.fun == "nonrob") acorf <- as.numeric(suppressWarnings(acf(x, plot = FALSE))$acf) else
+		if (acf.fun == "nonrob") acorf <- as.numeric(suppressWarnings(acf(x, plot = FALSE))$acf[-1]) else
 		acorf <- as.numeric(suppressWarnings(acfrob(x, fun = acf.fun, lag.max=truncation,..., plot = FALSE))$acf)
 		res <- robspec.acf(acorf, tmax = length(x),M=truncation,kernel)
 		if (var1) {sdestim <- function(y) {return(1)}} else {
